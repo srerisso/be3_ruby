6adaabf735b7698612fe4451d3f3254f5b51ce7b
Jose Manuel Gomez <jmgomezr@gmail.com>
2018-04-24 01:03:58 +0200

    Seeds db con formato correcto

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.new(email: 'jmgomezr@gmail.com', password: 'pastinaca');
+User.create(email: 'jmgomezr@gmail.com', password: 'pastinaca');
